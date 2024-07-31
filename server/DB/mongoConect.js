5663e50d2ae3b678bb6428ed3b73647dc461b07b
abigail <abigail506040@gmail.com>
2024-07-31 14:40:38 +0300

    mongfochang

--- server/DB/mongoConect.js
+++ server/DB/mongoConect.js
@@ -8,6 +8,6 @@ export async function connectToDB() {
     await mongoose.connect('mongodb://127.0.0.1:27017/wallpapere');
     console.log("MongoDB connected");
   } catch (err) {
-    console.error("Failed to connect to MongoDB", err);
+    console.error("Failed to connectbhjbj,b to MongoDB", err);
   }
 }

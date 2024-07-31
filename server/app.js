fc3dec4933581015d9b7265bb3220f97d290ad11
abigail <abigail506040@gmail.com>
2024-07-31 14:12:36 +0300

    144

--- server/app.js
+++ server/app.js
@@ -9,12 +9,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express()
 const port = 3000;
-
-
-
-
-
-
 app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")))
 images(app);
